728d8439d5c54864425dc3767fc9428e4812a9fc
jtown970 <jasonrtowner@gmail.com>
2020-05-11 18:53:23 -0700

    finished closures

--- closures.js
+++ closures.js
@@ -226,7 +226,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
